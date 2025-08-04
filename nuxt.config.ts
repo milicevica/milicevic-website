5ccc50bc6ae0eaae76d324fe02c6148adaadfebc
milicevica <hi@milicevic.dev>
2025-08-04 14:54:24 +0200

    setup eslint / lint-staged

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint']
-})
+  modules: ["@nuxt/content", "@nuxt/eslint"],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
