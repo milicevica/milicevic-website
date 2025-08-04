d0408ac2c594dc636e7106e6e017095ec2854fda
Aleksandar Milicevic <hi@milicevic.dev>
2025-08-04 15:57:55 +0200

    nuxt layout & page (#3)

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello World
-    </h1>
-    <button class="btn btn-secondary">
-      Secondary <Icon name="tabler:map" size="32" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
