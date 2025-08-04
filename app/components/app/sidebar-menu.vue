1a44641d5aeb2701d820c9470950d9045364c8ea
milicevica <hi@milicevic.dev>
2025-08-04 19:04:11 +0200

    mark current page

--- app/components/app/sidebar-menu.vue
+++ app/components/app/sidebar-menu.vue
@@ -1,13 +1,25 @@
 <template>
   <aside class="flex flex-col items-start text-sm text-gray-400 md:w-1/4">
     <nav class="space-y-2">
-      <NuxtLink to="/" class="text-emerald-500 block">
+      <NuxtLink
+        to="/"
+        class="block"
+        exact-active-class="text-emerald-500"
+      >
         About
       </NuxtLink>
-      <NuxtLink to="/blog" class="hover:text-white block">
+      <NuxtLink
+        to="/blog"
+        class="hover:text-white block"
+        exact-active-class="text-emerald-500"
+      >
         Blog
       </NuxtLink>
-      <NuxtLink to="/" class="hover:text-white block">
+      <NuxtLink
+        to="/random"
+        class="hover:text-white block"
+        exact-active-class="text-emerald-500"
+      >
         Random
       </NuxtLink>
     </nav>
