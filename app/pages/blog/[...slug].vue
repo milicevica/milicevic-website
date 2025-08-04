3b691b8f416dbed0c3837a047dddc070a7764512
milicevica <hi@milicevic.dev>
2025-08-04 18:52:48 +0200

    add content

---
+++ app/pages/blog/[...slug].vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const route = useRoute();
+const slug = route.params.slug || [];
+
+const { data: page } = await useAsyncData(() =>
+  queryCollection("content")
+    .path(`/${slug}`)
+    .first(),
+);
+</script>
+
+<template>
+  <ContentRenderer v-if="page" :value="page" />
+  <div v-else>
+    Blog post not found.
+  </div>
+</template>
