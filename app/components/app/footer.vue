f2cd3a030df21936883b6fccce683ade1ec8cf2b
milicevica <hi@milicevic.dev>
2025-08-05 12:31:00 +0200

    loop over array of socials

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -1,11 +1,24 @@
+<script setup>
+const socials = [
+  { name: "mail", link: "mailto:hi@milicevid.dev" },
+  { name: "brand-twitter", link: "https://x.com/milicev1ca" },
+  { name: "brand-github", link: "https://github.com/milicevica" },
+  { name: "brand-linkedin", link: "https://www.linkedin.com/in/milicevica/" },
+];
+</script>
+
 <template>
   <footer class="flex flex-row  items-center justify-between pt-8 text-xs text-gray-500">
     <p>© {{ new Date().getFullYear() }} Aleksandar Milicevic.</p>
     <div class="flex space-x-2 mt-2 text-lg text-gray-500">
-      <a href="mailto:hi@milicevid.dev"><Icon name="tabler:mail" /></a>
-      <a href="https://x.com/milicev1ca" target="_blank"><Icon name="tabler:brand-twitter" /></a>
-      <a href="https://github.com/milicevica" target="_blank"><Icon name="tabler:brand-github" /></a>
-      <a href="https://www.linkedin.com/in/milicevica/" target="_blank"><Icon name="tabler:brand-linkedin" /></a>
+      <a
+        v-for="social in socials"
+        :key="social.name"
+        :href="social.link"
+        target="_blank"
+      >
+        <Icon :name="`tabler:${social.name}`" />
+      </a>
     </div>
   </footer>
 </template>
