c558869feb092cc70dad3ca2cc8e173a51d26bb2
milicevica <hi@milicevic.dev>
2025-08-05 12:32:42 +0200

    loop over navigation items

--- app/components/app/sidebar-menu.vue
+++ app/components/app/sidebar-menu.vue
@@ -1,26 +1,22 @@
+<script setup>
+const navItems = [
+  { text: "About", link: "/" },
+  { text: "Blog", link: "/blog" },
+  { text: "Random", link: "/random" },
+];
+</script>
+
 <template>
   <aside class="flex flex-col items-start text-sm text-gray-400 md:w-1/4">
     <nav class="space-y-2">
       <NuxtLink
-        to="/"
+        v-for="item in navItems"
+        :key="item.text"
+        :to="item.link"
         class="block"
         exact-active-class="text-emerald-500"
       >
-        About
-      </NuxtLink>
-      <NuxtLink
-        to="/blog"
-        class="hover:text-white block"
-        exact-active-class="text-emerald-500"
-      >
-        Blog
-      </NuxtLink>
-      <NuxtLink
-        to="/random"
-        class="hover:text-white block"
-        exact-active-class="text-emerald-500"
-      >
-        Random
+        {{ item.text }}
       </NuxtLink>
     </nav>
   </aside>
