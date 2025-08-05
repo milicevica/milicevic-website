f09d5ba6f8b15c5a17307028c8c15de2cde7c793
milicevica <hi@milicevic.dev>
2025-08-05 12:28:37 +0200

    update social links

--- app/components/app/footer.vue
+++ app/components/app/footer.vue
@@ -2,10 +2,10 @@
   <footer class="flex flex-row  items-center justify-between pt-8 text-xs text-gray-500">
     <p>© {{ new Date().getFullYear() }} Aleksandar Milicevic.</p>
     <div class="flex space-x-2 mt-2 text-lg text-gray-500">
-      <a href="#"><Icon name="tabler:mail" /></a>
-      <a href="#"><Icon name="tabler:brand-twitter" /></a>
-      <a href="#"><Icon name="tabler:brand-github" /></a>
-      <a href="#"><Icon name="tabler:brand-linkedin" /></a>
+      <a href="mailto:hi@milicevid.dev"><Icon name="tabler:mail" /></a>
+      <a href="https://x.com/milicev1ca" target="_blank"><Icon name="tabler:brand-twitter" /></a>
+      <a href="https://github.com/milicevica" target="_blank"><Icon name="tabler:brand-github" /></a>
+      <a href="https://www.linkedin.com/in/milicevica/" target="_blank"><Icon name="tabler:brand-linkedin" /></a>
     </div>
   </footer>
 </template>
