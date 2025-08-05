f9469b9a63f839903e7ca34a506ab8bf4c4cfe36
milicevica <hi@milicevic.dev>
2025-08-05 13:25:58 +0200

    improve text in about me section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,10 +2,32 @@
   <div class="text-sm space-y-4">
     <p>I'm a developer living in Belgrade, Serbia.</p>
 
-    <p>Currently I spend most of my time working as a Software Engineer at <a href="#" class="text-emerald-500 underline">Deluxe</a>, on a print on demand ecommerce project.</p>
+    <p>
+      For the last 7+ years, I've spent most of my time working as a Software Engineer at
+      <a
+        href="https://www.deluxe.com/"
+        target="_blank"
+        class="text-emerald-500 underline"
+      >Deluxe</a>. I'm currently working on a project in the print-on-demand industry, where we're building an e-commerce solution for other businesses.
+    </p>
 
     <p>
-      In my free time I play racing simulations. I currently play F1 2025. 🏎️
+      In my free time, I enjoy playing racing simulation games as a way to relax and have fun. I'm still new to the hobby and currently learning while playing
+      <a
+        href="https://www.ea.com/games/f1/f1-25"
+        target="_blank"
+        class="text-emerald-500 underline"
+      >
+        F1 2025
+      </a>
+      using a
+      <a
+        href="https://www.logitechg.com/en-us/products/driving/driving-force-racing-wheel.html"
+        target="_blank"
+        class="text-emerald-500 underline"
+      >
+        Logitech G29.
+      </a>
     </p>
   </div>
 </template>
